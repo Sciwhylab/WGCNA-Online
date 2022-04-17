4b50d49ec35e7c913e278b884deba7f6064dfead
kitswas <90329875+kitswas@users.noreply.github.com>
2022-04-17 16:05:01 +0530

    Created WGCNA Shiny module

--- app.R
+++ app.R
@@ -13,46 +13,55 @@ Database <- read.delim(path_to_file, row.names = 1)
 # Define UI for application
 ui <- fluidPage(
 	# Theme
-	theme = bs_theme(
-		bootswatch = "minty"
-	), 
+	theme = bs_theme(bootswatch = "minty"),
 	# # Favicon
 	# tags$head(tags$link(rel="shortcut icon", href=here::here("www", "favicon.ico"), type="image/x-icon")),
 	# Application title
-	titlePanel("RefEx_expression_EST10_human"), 
+	titlePanel("WGCNA Analysis"),
 	# The data table
-	DTOutput("GeneTable")
+	h1("Dataset"),
+	DTOutput("GeneTable"),
+	
+	WGCNAShinyUI("1")
 )
 
 # Define server logic
 server <- function(input, output) {
-	
-	output$GeneTable <- renderDT(Database, 
-								 extensions = c("FixedColumns", "Buttons"), 
-								 rownames = FALSE, 
-								 options = list(
-								 	searching = TRUE,
-								 	searchHighlight = TRUE,
-								 	autoWidth = TRUE,
-								 	paging = TRUE,
-								 	lengthMenu = matrix(c(10, 25, 50, -1, 10, 25, 50, "All"), ncol = 4, nrow = 2, byrow = TRUE),
-								 	pagingType = "full_numbers",
-								 	scroller = TRUE,
-								 	scrollX = TRUE,
-								 	scrollY = "900px",
-								 	scrollCollapse = TRUE,
-								 	fixedHeader = TRUE,
-								 	class = 'cell-border stripe',
-								 	# fixedColumns = list( # unnecessary because the 
-								 	# 	leftColumns = 1,
-								 	# 	heightMatch = 'none'
-								 	# ),
-								 	buttons = c('copy', 'csv', 'excel', 'pdf', 'print'), 
-								 	dom = 'lfrtiBp'
-								 ), 
-								 server = TRUE
+	output$GeneTable <- renderDT(
+		Database,
+		extensions = c("FixedColumns", "Buttons"),
+		rownames = FALSE,
+		options = list(
+			searching = TRUE,
+			searchHighlight = TRUE,
+			autoWidth = TRUE,
+			paging = TRUE,
+			lengthMenu = matrix(
+				c(10, 25, 50,-1, 10, 25, 50, "All"),
+				ncol = 4,
+				nrow = 2,
+				byrow = TRUE
+			),
+			pagingType = "full_numbers",
+			scroller = TRUE,
+			scrollX = TRUE,
+			scrollY = "900px",
+			scrollCollapse = TRUE,
+			fixedHeader = TRUE,
+			class = 'cell-border stripe',
+			# fixedColumns = list( # unnecessary because the
+			# 	leftColumns = 1,
+			# 	heightMatch = 'none'
+			# ),
+			buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+			dom = 'lfrtiBp'
+		),
+		server = TRUE
 	)
+	WGCNAShiny("1")
 }
 
-# Run the application 
-runApp(shinyApp(ui = ui, server = server), host = "0.0.0.0", port = 4330)
+# Run the application
+runApp(shinyApp(ui = ui, server = server),
+	   host = "0.0.0.0",
+	   port = 4330)
