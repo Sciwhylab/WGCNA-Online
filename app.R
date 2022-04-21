2f21b2ccfc60d6542370597e752370850da6aee5
kitswas <90329875+kitswas@users.noreply.github.com>
2022-04-21 23:35:13 +0530

    Added about

--- app.R
+++ app.R
@@ -7,7 +7,9 @@ library(bslib)
 # This sets the application-scoped cache to be a disk
 # cache that can be shared among multiple concurrent R processes, and is
 # deleted when the system reboots.
-shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), "WGCNA-cache")))
+shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir(
+	
+)), "WGCNA-cache")))
 
 path_to_file <-  here::here("RefEx_expression_EST10_human.tsv")
 
@@ -22,7 +24,8 @@ ui <- fluidPage(# Theme
 		tags$head(
 			tags$meta(charset = "utf-8"),
 			tags$meta(name = "description", content = "A Shiny App for WGCNA Analysis"),
-			tags$meta(name = "robots", content = "noindex"), # Prevents app from showing up in search results
+			tags$meta(name = "robots", content = "noindex"),
+			# Prevents app from showing up in search results
 			# Favicon
 			tags$link(
 				rel = "shortcut icon",
@@ -34,6 +37,24 @@ ui <- fluidPage(# Theme
 		tags$body(
 			# Application title
 			titlePanel("WGCNA Analysis"),
+			h1("About"),
+			div(
+				p(
+					"This is an online application that lets users perform",
+					a("weighted gene co-expression network analysis", href = "https://en.wikipedia.org/wiki/Weighted_correlation_network_analysis"),
+					"on their data."
+				),
+				p("To know more, take a look at the example below."),
+				p(
+					"It uses the R package ",
+					a("WGCNA.", href = "https://horvath.genetics.ucla.edu/html/CoexpressionNetwork/Rpackages/WGCNA/"),
+					"A choice has been provided to the user in some cases. All other parameters have been left to their recommended defaults. "
+				),
+				p(
+					"This is a ",
+					a("Shiny Application.", href = "https://shiny.rstudio.com/")
+				)
+			),
 			# The data table
 			h1("Dataset"),
 			DTOutput("GeneTable"),
@@ -41,8 +62,7 @@ ui <- fluidPage(# Theme
 			WGCNAShinyUI("1")
 		),
 		lang = "en"
-	)
-)
+	))
 
 # Define server logic
 server <- function(input, output) {
